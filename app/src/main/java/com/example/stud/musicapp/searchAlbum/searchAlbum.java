9669e063113719b6006b91c7aaedb662b20cee48
W57004 <mr.wodus@gmail.com>
2018-06-14 18:43:14 +0200

    feature/search-album

--- app/src/main/java/com/example/stud/musicapp/searchAlbum/searchAlbum.java
+++ app/src/main/java/com/example/stud/musicapp/searchAlbum/searchAlbum.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.telecom.Call;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +21,8 @@ import com.example.stud.musicapp.api.SearchAlbum;
 import com.example.stud.musicapp.api.SearchAlbums;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import retrofit2.Callback;
@@ -93,13 +98,14 @@ public class searchAlbum extends AppCompatActivity {
 
                 Toast.makeText(searchAlbum.this, "Znaleziono " + searchAlbums.album.size() + " wyników", Toast.LENGTH_SHORT).show();
 
-                private void updateList(SearchAlbums searchAlbums) {
-                    albums.clear();
-                    albums.addAll(searchAlbums.album);
 
-                    rvList.getAdapter().notifyDataSetChanged();
-                }
 
+            }
+            private void updateList(SearchAlbums searchAlbums) {
+                albums.clear();
+                albums.addAll(searchAlbums.album);
+
+                rvList.getAdapter().notifyDataSetChanged();
             }
 
             @Override
@@ -114,4 +120,99 @@ public class searchAlbum extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sort_albums_menu, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSortName:
+                sortByName();
+                return true;
+            case R.id.itemSortYear:
+                sortByYear();
+                return true;
+            case R.id.itemSortSales:
+                sortBySales();
+                return true;
+            case R.id.itemSortScore:
+                sortByScore();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sortByName() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.strAlbum.equals(album2.strAlbum)) {
+                    return 0;
+                }
+
+                return album1.strAlbum.compareTo(album2.strAlbum);
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+
+    private void sortByYear() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intYearReleased == album2.intYearReleased) {
+                    return 0;
+                }
+
+                return album1.intYearReleased < album2.intYearReleased ? -1 : 1;
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+
+    private void sortBySales() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intSales == album2.intSales) {
+                    return 0;
+                }
+
+                return album1.intSales < album2.intSales ? -1 : 1;
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+
+    private void sortByScore() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intScore == album2.intScore) {
+                    return 0;
+                }
+
+                return album1.intScore < album2.intScore ? -1 : 1;
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+
+
+
+
+
 }
