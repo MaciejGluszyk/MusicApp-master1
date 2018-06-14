f36fdfe623457e95613abac860ad048ec572b839
W57004 <mr.wodus@gmail.com>
2018-06-14 18:06:55 +0200

    feature/search-album.

--- app/src/main/java/com/example/stud/musicapp/searchAlbum/searchAlbum.java
+++ app/src/main/java/com/example/stud/musicapp/searchAlbum/searchAlbum.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.telecom.Call;
 import android.view.View;
@@ -16,6 +17,9 @@ import com.example.stud.musicapp.api.ApiService;
 import com.example.stud.musicapp.api.SearchAlbum;
 import com.example.stud.musicapp.api.SearchAlbums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Callback;
 import retrofit2.Response;
 
@@ -25,6 +29,7 @@ public class searchAlbum extends AppCompatActivity {
     RecyclerView rvList;
 
     SharedPreferences sharedPreferences;
+    List<SearchAlbum> albums = new ArrayList<>(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,12 @@ public class searchAlbum extends AppCompatActivity {
 
         etQuery = findViewById(R.id.etQuery);
         rvList = findViewById(R.id.rvList);
+
+        SearchAlbumAdapter searchAlbumAdapter = new SearchAlbumAdapter(albums);
+        rvList.setAdapter(searchAlbumAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvList.setLayoutManager(linearLayoutManager);
 
         String artist = sharedPreferences.getString("query", null);
         etQuery.setText(artist);
@@ -81,6 +92,14 @@ public class searchAlbum extends AppCompatActivity {
                 }
 
                 Toast.makeText(searchAlbum.this, "Znaleziono " + searchAlbums.album.size() + " wyników", Toast.LENGTH_SHORT).show();
+
+                private void updateList(SearchAlbums searchAlbums) {
+                    albums.clear();
+                    albums.addAll(searchAlbums.album);
+
+                    rvList.getAdapter().notifyDataSetChanged();
+                }
+
             }
 
             @Override
